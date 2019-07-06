5b3d312e5d1149c69b120e24c458bd1fbcd7c69e
ST-19 <44902884+ST-19@users.noreply.github.com>
2019-07-05 21:39:40 -0500

    Rename Tierra.cpp to Tierra.h

---
+++ Tierra.h
@@ -0,0 +1,35 @@
+#ifndef AGREGACION_TIERRA_H
+#define AGREGACION_TIERRA_H
+
+#include <iostream>
+#include <vector>
+#include "Tipos.h"
+#include "Objeto.h"
+
+using namespace std;
+
+// Valores constantes
+const TipoEntero ALTURA = 21;
+const TipoEntero ANCHO  = 21;
+const TipoCaracter COLOR ='.';
+
+class Tierra {
+protected:
+    vector<vector<char>> plano;
+    vector<Objeto*> objetos;
+public:
+    Tierra();
+    Tierra(TipoEntero altura, TipoEntero ancho);
+    virtual ~Tierra();
+    void adicionarObjeto(Objeto* objeto);
+    Objeto* removerObjeto(string& nombre);
+    void imprimirObjetos();
+    TipoEntero getAltura();
+    TipoEntero getAncho();
+    TipoEntero getCantidadObjectos();
+    void dibujarTierra();
+    void actualizarTierra();
+
+};
+
+#endif //AGREGACION_TIERRA_H
