c3e26d2b20cdf0dbb73d5eb0f5a300ec7cc1a404
ST-19 <44902884+ST-19@users.noreply.github.com>
2019-07-05 21:38:57 -0500

    Create Tierra.cpp

---
+++ Tierra.cpp
@@ -0,0 +1,91 @@
+#include "Tierra.h"
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include "Menu.h"
+
+using namespace std;
+
+Tierra::Tierra() {
+    plano.resize(ALTURA);
+    for (auto& item: plano)
+        item.resize(ANCHO);
+}
+
+Tierra::Tierra(TipoEntero altura, TipoEntero ancho) {
+    plano.resize(altura);
+    for (auto& item: plano)
+        item.resize(ancho);
+}
+
+Tierra::~Tierra() {}
+
+void Tierra::adicionarObjeto(Objeto* objeto) {
+    objetos.emplace_back(objeto);
+}
+
+Objeto* Tierra::removerObjeto(string& nombre) {
+    // Buscando objeto
+    if (objetos.size() == 0)
+        return nullptr;
+
+    auto iter = find_if(begin(objetos), end(objetos),
+                        [&nombre](Objeto* obj){ return obj->getNombre() == nombre; });
+    if (iter == end(objetos))
+        return nullptr;
+    // Eliminando objeto
+    objetos.erase(iter);
+    //-- si encuentra al objeto lo separa del vector, pero no mata al objeto, esto se hara en el menu.
+    return *iter;
+}
+
+void Tierra::imprimirObjetos() {
+    int i = 0;
+    for (auto& item: objetos) {
+        cout << "* * * * * * [" << i << "] "<<endl;
+        cout << "Nombre = " << item->getNombre()<<endl;
+        cout<<item->mostrarPosicion()<<endl;
+        cout<< "Tipo = " << item->getColor() << '\n';
+        cout<< "Telefono = " << item->getNumeroTelefono() << '\n';
+
+        i++;
+    }
+}
+
+void Tierra::actualizarTierra() {
+    for (auto &row: plano)
+        for (auto &cell: row)
+            cell = COLOR;
+
+    for (auto& item: objetos)
+        plano[item->getPosX()][item->getPosY()]
+                = item->getColor();
+}
+
+void Tierra::dibujarTierra() {
+    cout << '\n';
+    cout << setw(3) << ' ';
+    for (auto j = 0; j < getAncho(); ++j)
+        cout << setw(3) << j;
+    cout << '\n';
+
+    for (auto i = 0; i < getAltura(); ++i) {
+        cout << setw(3) << i;
+        for (auto j = 0; j < getAncho(); ++j) {
+            cout << setw(3) << plano[i][j];
+        }
+        cout << '\n';
+    }
+}
+
+TipoEntero Tierra::getAltura() {
+    return plano.size();
+}
+
+TipoEntero Tierra::getAncho(){
+    return plano[0].size();
+}
+
+TipoEntero Tierra::getCantidadObjectos() {
+    return 0;
+}
